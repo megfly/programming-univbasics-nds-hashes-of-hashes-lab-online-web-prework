8c67094e0372427730e65d117499e04070e51da8
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-15 22:09:34 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,12 +43,25 @@ def naming_system
   # HoH all the way to Species!
   
 naming_system = {
-  :Kingdom => {
-    :Phylum => {
-      :Class => {}
-    }
+  label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+  sub_category: {
+    label: "Class",
+  sub_category: {
+    label: "Order",
+  sub_category: {
+    label: "Family",
+  sub_category: {
+    label: "Genus",
+  sub_category: {
+    label: "Species"
+  }
+  }
+  }
+  }
+  }
   }
 }
-
 end 
 
